4812cc24a64af037dd2e8658899eee73b4808489
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-06-02 13:59:49 -0500

    Add and complete challenge 5

---
+++ challenge5.rb
@@ -0,0 +1,17 @@
+# have an array of travel destinations
+# alphabetize that array
+# we want to print a sentence that have each travel destination in it
+
+places = ["Japan", "Rome", "New Zealand", "Korea"]
+
+places.sort.each do |place|
+    p "The next place I want to travel is #{place}!"
+end
+
+# sort_place = places.sort
+
+# sort_place.each do |place|
+#     p "The next place I want to travel is #{place}!"
+# end
+
+
